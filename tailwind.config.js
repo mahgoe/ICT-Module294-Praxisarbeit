5c8f62d6be7bd5f75fa0ddd3486106e71e59ea1a
mahgoe <gonenmahir@hotmail.com>
2023-08-24 01:08:15 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"], // or 'src/**/*.html' or 'src/**/*.vue'
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
