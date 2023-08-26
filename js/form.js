ec7ed77d7d773d4af81fc86495cbbb7b28befb27
Bobby <138381572+BiluliB@users.noreply.github.com>
2023-08-26 10:54:06 +0200

    changed dropdown and add validation

--- js/form.js
+++ js/form.js
@@ -1,39 +1,7 @@
-const dropdownButton = document.getElementById("dropdownDisplayButton");
-const dropdownMenu = document.getElementById("dropdownMenu");
-const hiddenSelect = document.getElementById("hiddenSelect");
-const optionItems = document.querySelectorAll(".optionItem");
 const params = new URLSearchParams(window.location.search);
 const dropdownValue = params.get("service");
 
 if (dropdownValue) {
-  const elm = document.querySelector(`li[data-value="${dropdownValue}"]`);
-  if (elm) {
-    setActive(elm);
-  }
+  const selectElement = document.querySelector("select");
+  selectElement.value = dropdownValue;
 }
-
-dropdownButton.addEventListener("click", () => {
-  dropdownMenu.classList.toggle("hidden");
-});
-
-optionItems.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    console.log(e);
-    setActive(e.target);
-  });
-});
-
-function setActive(target) {
-  const value = target.getAttribute("data-value");
-  const text = target.textContent;
-  hiddenSelect.value = value;
-  document.getElementById("dropdownDisplay").textContent = text;
-  dropdownMenu.classList.add("hidden");
-}
-
-document.querySelector("form").addEventListener("submit", function (e) {
-  if (!hiddenSelect.value || hiddenSelect.value === "") {
-    e.preventDefault();
-    alert("Bitte wählen Sie eine Dienstleistung aus");
-  }
-});
