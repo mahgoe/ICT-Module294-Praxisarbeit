d7e252bdf4712e215cbb1ab85a57e1a830434d00
Bobby <138381572+BiluliB@users.noreply.github.com>
2023-08-30 11:53:30 +0200

    add start-,enddate select, add prizes, calc select

--- js/calendar.js
+++ js/calendar.js
@@ -353,7 +353,7 @@
           maxDate: null,
           maxNumberOfDates: 1,
           maxView: 3,
-          minDate: null,
+          minDate: new Date(),
           nextArrow:
             '<svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>',
           orientation: "auto",
@@ -367,7 +367,7 @@
           title: "",
           todayBtn: false,
           todayBtnMode: 0,
-          todayHighlight: false,
+          todayHighlight: true,
           updateOnBlur: true,
           weekStart: 0,
         };
--- js/form.js
+++ js/form.js
@@ -6,12 +6,10 @@ if (dropdownValue) {
   selectElement.value = dropdownValue;
 }
 
-// Funktion, um das voraussichtliche Abholdatum basierend auf der gewählten Priorität zu setzen
-function setEstimatedPickupDate(priority) {
-  const today = new Date();
+function setEstimatedPickupDate(priority, startDate) {
+  let start = new Date(startDate);
   let daysToAdd = 0;
 
-  // Die Anzahl der hinzuzufügenden Tage basierend auf der Priorität
   if (priority === "low") {
     daysToAdd = 12;
   } else if (priority === "standard") {
@@ -20,28 +18,101 @@ function setEstimatedPickupDate(priority) {
     daysToAdd = 5;
   }
 
-  // Das neue Datum berechnen
-  const newDate = new Date();
-  newDate.setDate(today.getDate() + daysToAdd);
+  const newDate = new Date(start);
+  newDate.setDate(start.getDate() + daysToAdd);
 
-  // Das Datum im gewünschten Format formatieren: DD/MM/YYYY
   const formattedDate = `${String(newDate.getDate()).padStart(2, "0")}/${String(
     newDate.getMonth() + 1
   ).padStart(2, "0")}/${newDate.getFullYear()}`;
 
-  // Das voraussichtliche Abholdatum im Textfeld setzen
-  document.getElementById("calendar").value = formattedDate;
+  document.getElementById("endDate").value = formattedDate;
 }
 
-// Funktion, die beim Laden der Seite aufgerufen wird
-window.onload = function () {
-  // Das voraussichtliche Abholdatum für die Standardpriorität 'Tief' setzen
-  setEstimatedPickupDate("low");
+function calculateTotal() {
+  let serviceCost = 0;
+  let priorityCost = 0;
 
-  // Event-Listener für Änderungen der Radio-Buttons hinzufügen
+  const serviceOption = document.querySelector("select").value;
+  if (serviceOption === "kleinerService") {
+    serviceCost = 49;
+  } else if (serviceOption === "grosserService") {
+    serviceCost = 69;
+  } else if (serviceOption === "rennskiService") {
+    serviceCost = 99;
+  }
+  // ... (weitere Optionen)
+
+  const priority = document.querySelector(
+    'input[name="list-radio"]:checked'
+  ).value;
+  if (priority === "standard") {
+    priorityCost = 5;
+  } else if (priority === "express") {
+    priorityCost = 10;
+  }
+
+  const total = serviceCost + priorityCost;
+  document.getElementById("total").value = `CHF ${total}.-`;
+}
+
+window.onload = function () {
+  const startDateInput = document.getElementById("startDate");
+  const today = new Date();
+  const formattedToday = `${String(today.getDate()).padStart(2, "0")}/${String(
+    today.getMonth() + 1
+  ).padStart(2, "0")}/${today.getFullYear()}`;
+
+  startDateInput.value = formattedToday;
+  setEstimatedPickupDate("low", today);
+
   document.querySelectorAll('input[name="list-radio"]').forEach((radio) => {
     radio.addEventListener("change", function () {
-      setEstimatedPickupDate(this.value);
+      const startDateValue = startDateInput.value.split("/");
+      const startDate = new Date(
+        `${startDateValue[2]}-${startDateValue[1]}-${startDateValue[0]}`
+      );
+      setEstimatedPickupDate(this.value, startDate);
+      calculateTotal();
     });
   });
+
+  startDateInput.addEventListener("change", function () {
+    const selectedDateValue = this.value.split("/");
+    const selectedDate = new Date(
+      `${selectedDateValue[2]}-${selectedDateValue[1]}-${selectedDateValue[0]}`
+    );
+    const priority = document.querySelector(
+      'input[name="list-radio"]:checked'
+    ).value;
+    setEstimatedPickupDate(priority, selectedDate);
+  });
+
+  // Event-Listener für Änderungen an der Service-Dropdown-Auswahl
+  document.querySelector("select").addEventListener("change", function () {
+    calculateTotal();
+  });
+
+  // Startwert für den Gesamtbetrag setzen
+  calculateTotal();
 };
+
+let lastKnownStartDate = "";
+
+setInterval(() => {
+  const startDateInput = document.getElementById("startDate");
+  const currentStartDate = startDateInput.value;
+
+  if (currentStartDate !== lastKnownStartDate) {
+    lastKnownStartDate = currentStartDate;
+
+    const startDateValue = currentStartDate.split("/");
+    const startDate = new Date(
+      `${startDateValue[2]}-${startDateValue[1]}-${startDateValue[0]}`
+    );
+    const priority = document.querySelector(
+      'input[name="list-radio"]:checked'
+    ).value;
+
+    setEstimatedPickupDate(priority, startDate);
+  }
+}, 500); // alle 500 Millisekunden
