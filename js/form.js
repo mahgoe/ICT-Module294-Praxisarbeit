85aa637aafc6765cb41390b5b123b5c4251575e0
mahgoe <gonenmahir@hotmail.com>
2023-09-01 19:23:30 +0200

    init backend, bugfixes

--- js/form.js
+++ js/form.js
@@ -1,3 +1,125 @@
+function validateForm(e) {
+  e.preventDefault(); // Verhindert den Standard-Submit des Formulars
+
+  // Elemente aus dem Formular holen
+  const firstName = document.getElementById("firstname");
+  const lastName = document.getElementById("lastname");
+  const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
+
+  // Validierung
+  let isValid = true;
+
+  if (!validateRequiredField(firstName) || !validateNoWhitespace(firstName)) {
+    isValid = false;
+    document.getElementById("errorMessageFirstName").innerText =
+      "Leerzeichen am Anfang oder Ende sind nicht erlaubt im Vornamen.";
+  } else {
+    document.getElementById("errorMessageFirstName").innerText = "";
+  }
+
+  if (!validateRequiredField(lastName) || !validateNoWhitespace(lastName)) {
+    isValid = false;
+    document.getElementById("errorMessageLastName").innerText =
+      "Leerzeichen am Anfang oder Ende sind nicht erlaubt im Nachnamen.";
+  } else {
+    document.getElementById("errorMessageLastName").innerText = "";
+  }
+
+  if (
+    !validateRequiredField(email) ||
+    !validateNoWhitespace(email) ||
+    !validateEmailFormat(email)
+  ) {
+    isValid = false;
+    document.getElementById("errorMessageEmail").innerText =
+      "Falsche E-Mail-Formatierung";
+  } else {
+    document.getElementById("errorMessageEmail").innerText = "";
+  }
+
+  if (!validateRequiredField(phone) || !validatePhoneNumber(phone)) {
+    isValid = false;
+    document.getElementById("errorMessagePhone").innerText =
+      "Falsche Telefonnummer-Formatierung";
+  } else {
+    document.getElementById("errorMessagePhone").innerText = "";
+  }
+
+  // Wenn alle Validierungen erfolgreich sind, dann Formular verarbeiten
+  if (isValid) {
+    postData(firstName.value, lastName.value, email.value);
+    console.log(
+      "firstName: " + firstName.value,
+      "lastName: " + lastName.value,
+      "email: " + email.value
+    );
+  }
+}
+
+// Funktionen für Validierungen und Formularverarbeitung
+function validateRequiredField(field) {
+  if (field.value === "") {
+    field.classList.add("error");
+    return false;
+  } else {
+    field.classList.remove("error");
+    return true;
+  }
+}
+
+function validateNoWhitespace(field) {
+  const textPattern = /^\s|\s$/;
+  if (textPattern.test(field.value)) {
+    return false;
+  }
+  return true;
+}
+
+function validateEmailFormat(field) {
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!emailPattern.test(field.value)) {
+    return false;
+  }
+  return true;
+}
+
+function validatePhoneNumber(field) {
+  const phonePattern =
+    /(\b(0041|0)|\B\+41)(\s?\(0\))?(\s)?[1-9]{2}(\s)?[0-9]{3}(\s)?[0-9]{2}(\s)?[0-9]{2}\b/;
+  if (!phonePattern.test(field.value)) {
+    return false;
+  }
+  return true;
+  console.log("Telefon richtig");
+}
+
+function postData(firstName, lastName, email) {
+  const post = {
+    name: firstName + " " + lastName,
+    email: email,
+    phone: document.getElementById("phone").value,
+    priority: document.querySelector('input[name="list-radio"]:checked').value,
+    create_date: document.getElementById("startDate").value,
+    pickup_date: document.getElementById("endDate").value,
+  };
+
+  fetch("http://localhost:5000/api/registration", {
+    method: "POST",
+    body: JSON.stringify({
+      name: post.name,
+      email: post.email,
+      phone: post.phone,
+      priority: post.priority,
+      create_date: post.create_date,
+      pickup_date: post.pickup_date,
+    }),
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(JSON.stringify(json)));
+}
+
 const params = new URLSearchParams(window.location.search);
 const dropdownValue = params.get("service");
 
@@ -10,11 +132,11 @@ function setEstimatedPickupDate(priority, startDate) {
   let start = new Date(startDate);
   let daysToAdd = 0;
 
-  if (priority === "low") {
+  if (priority === "Tief") {
     daysToAdd = 12;
-  } else if (priority === "standard") {
+  } else if (priority === "Standard") {
     daysToAdd = 7;
-  } else if (priority === "express") {
+  } else if (priority === "Express") {
     daysToAdd = 5;
   }
 
@@ -51,9 +173,9 @@ function calculateTotal() {
   const priority = document.querySelector(
     'input[name="list-radio"]:checked'
   ).value;
-  if (priority === "standard") {
+  if (priority === "Standard") {
     priorityCost = 5;
-  } else if (priority === "express") {
+  } else if (priority === "Express") {
     priorityCost = 10;
   }
 
@@ -62,6 +184,10 @@ function calculateTotal() {
 }
 
 window.onload = function () {
+  document
+    .getElementById("submitForm")
+    .addEventListener("submit", validateForm);
+
   const startDateInput = document.getElementById("startDate");
   const today = new Date();
   const formattedToday = `${String(today.getDate()).padStart(2, "0")}/${String(
@@ -69,10 +195,11 @@ window.onload = function () {
   ).padStart(2, "0")}/${today.getFullYear()}`;
 
   startDateInput.value = formattedToday;
-  setEstimatedPickupDate("low", today);
+  setEstimatedPickupDate("Tief", today);
 
   document.querySelectorAll('input[name="list-radio"]').forEach((radio) => {
     radio.addEventListener("change", function () {
+      console.log("radio changed to " + this.value);
       const startDateValue = startDateInput.value.split("/");
       const startDate = new Date(
         `${startDateValue[2]}-${startDateValue[1]}-${startDateValue[0]}`
